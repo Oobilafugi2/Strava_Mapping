f7e33cac28d588b6e751305afc12b42ba770b906
Kevin <kevinjervis17@gmail.com>
2023-02-18 08:38:44 -0800

    S/B/R 200 Activities Layers

--- Folium.py
+++ Folium.py
@@ -12,6 +12,12 @@ auth_url = "https://www.strava.com/oauth/token"
 # accessing activity
 activities_url = "https://www.strava.com/api/v3/athlete/activities"
 
+# accessing club activities
+club_link = "https://www.strava.com/api/v3/clubs/807553/activities"
+
+# accessing club activities
+gpx_link = "https://www.strava.com/api/v3/routes/id/export_gpx"
+
 # json formatted data for personalized Strava authentication
 payload = {
     'client_id': "101716",
@@ -29,40 +35,80 @@ access_token = res.json()['access_token']
 
 # requesting to get data from activities using the refreshed access key
 header = {'Authorization': 'Bearer ' + access_token}
-param = {'per_page': 50, 'page': 1}
+param = {'per_page': 200, 'page': 1}
 my_dataset = requests.get(activities_url, headers=header, params=param).json()
 
-# print(my_dataset[2])
 
-# initialize folium map, set basemap and starting location
-m = folium.Map(tiles='cartodbdark_matter', location=[37.9101, -122.0652])
+# initialize folium map, set default map to blank so all the custom maps on top take precedent
+m = folium.Map(tiles=None, location=[37.9101, -122.0652])
+folium.raster_layers.TileLayer(tiles='cartodbdark_matter', name='Triathlon Training Routes').add_to(m)
+
+# title_html = '''
+#              <h3 align="center" style="font-size:20px"><b>Your map title</b></h3>
+#              '''
+# m.get_root().html.add_child(folium.Element(title_html))
+
+# initialize layers where routes will be stored
+fg_rides = folium.FeatureGroup("Rides")
+fg_runs = folium.FeatureGroup("Runs")
+fg_swims = folium.FeatureGroup("Swims")
 
 # loop through the full dataset
 for i in range(len(my_dataset)):
-
     # Separate runs and rides to give them each different properties
     if my_dataset[i]["type"] == 'Ride':
         
-        # the "try" gets around activities that have no polyline
-        # the block decodes the polyline into coordinates, and adds that and the activity name to the map
+         # the "try" gets around activities that have no polyline
+         # the block decodes the polyline into coordinates, and adds that and the activity name to the map
         try:
             route = polyline.decode(my_dataset[i]['map']['summary_polyline'])
+
+            map_id = my_dataset[i]['map']['id']
+            
             popup_text = my_dataset[i]['name']
             popup = folium.Popup(html=popup_text)
-            my_Polyline = folium.PolyLine(locations=route,weight=3, popup=popup,)
-            m.add_child(my_Polyline)
+
+            # layer option
+            folium.PolyLine(locations=route, weight=3, popup=popup).add_to(fg_rides)
+
+            # non-layer option
+            # my_Polyline = folium.PolyLine(locations=route,weight=3, popup=popup,)
+            # m.add_child(my_Polyline)
         except ValueError:
             print("no polyline")
+        except KeyError:
+            print("no polyline")
+
     if my_dataset[i]["type"] == 'Run':
         try:
             route = polyline.decode(my_dataset[i]['map']['summary_polyline'])
+            
             popup_text = my_dataset[i]['name']
             popup = folium.Popup(html=popup_text)
-            my_Polyline = folium.PolyLine(locations=route,weight=3, color="red", popup=popup)
-            m.add_child(my_Polyline)
+
+            folium.PolyLine(locations=route, weight=3, color="red", popup=popup).add_to(fg_runs)
+            
         except ValueError:
             print("no polyline")
 
+    if my_dataset[i]["type"] == 'Swim':
+        try:
+            route = polyline.decode(my_dataset[i]['map']['summary_polyline'])
+            
+            popup_text = my_dataset[i]['name']
+            popup = folium.Popup(html=popup_text)
+
+            folium.PolyLine(locations=route, weight=3, color="green", popup=popup).add_to(fg_swims)
+            
+        except ValueError:
+            print("no polyline")
+
+# add each layer to blank map
+fg_rides.add_to(m)
+fg_runs.add_to(m)  
+fg_swims.add_to(m) 
+
+folium.LayerControl().add_to(m)
         
 # pushes map to html file where it can be displayed
 m.save(r"C:\Users\KevinJ\Strava_Mapping\index.html")
